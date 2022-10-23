dcad70ddb5f81b4f38f13bdf9d271b8772726356
ZyperX <59225387+ZyperX@users.noreply.github.com>
2022-10-23 10:26:24 +0530

    Add files via upload

---
+++ cmd/zbxss/main.go
@@ -0,0 +1,50 @@
+package main
+import "os"
+import "fmt"
+import "bufio"
+import "strings"
+import "github.com/valyala/fasthttp"
+
+var (
+headers = [...]string{
+		"X-Originating-IP",
+		"X-Forwarded-For",
+                "X-Forwarded",
+                "Forwarded-For",
+                "X-Remote-IP",
+                "X-Remote-Addr",
+                "X-ProxyUser-Ip",
+                "X-Original-URL",
+                "Client-IP",
+                "True-Client-IP",
+                "Cluster-Client-IP",
+                "X-ProxyUser-Ip",
+                "X-Host",
+                "X-Forwarded-Server",
+                "X-HTTP-Host-Override",
+               }
+)
+
+
+func probes (payload string,url string) {
+        req := fasthttp.AcquireRequest()
+        req.SetRequestURI(url)
+     for _,e := range headers{
+        req.Header.Add(e, payload)
+        }
+        resp := fasthttp.AcquireResponse()
+        client := &fasthttp.Client{}
+        client.Do(req, resp)
+        sc := resp.StatusCode()
+        fmt.Println(url,":",sc)
+}
+
+func main() {
+scanner := bufio.NewScanner(os.Stdin)
+payload := os.Args[1]
+for scanner.Scan() {
+		target := strings.TrimSpace(scanner.Text())
+                probes(payload,target)
+}
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ZyperX/simple-go-service
+
+go 1.19
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.40.0 // indirect
+)
