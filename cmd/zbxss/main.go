bba28d9632c664c109cd8d6aebfc63e8c6d21845
root <rishikesh2277@gmail.com>
2022-10-26 10:27:25 +0400

    format

--- cmd/zbxss/main.go
+++ cmd/zbxss/main.go
@@ -1,51 +1,7 @@
 package main
-import "os"
-import "fmt"
-import "bufio"
-import "strings"
-import "github.com/valyala/fasthttp"
 
-var (
-headers = [...]string{
-		"X-Originating-IP",
-		"X-Forwarded-For",
-                "X-Forwarded",
-                "Forwarded-For",
-                "X-Remote-IP",
-                "X-Remote-Addr",
-                "X-ProxyUser-Ip",
-                "X-Original-URL",
-                "Client-IP",
-                "True-Client-IP",
-                "Cluster-Client-IP",
-                "X-ProxyUser-Ip",
-                "X-Host",
-                "X-Forwarded-Server",
-                "X-HTTP-Host-Override",
-               }
-)
-
-
-func probes (payload string,url string) {
-        req := fasthttp.AcquireRequest()
-        req.SetRequestURI(url)
-     for _,e := range headers{
-        req.Header.Add(e, payload)
-        }
-        resp := fasthttp.AcquireResponse()
-        client := &fasthttp.Client{}
-        statusCode, _, _ := client.Get(nil, url)
-        client.Do(req, resp)
-        sc := resp.StatusCode()
-        fmt.Println(url,"BXss payload send:",sc,"hostheader check:",statusCode)
-}
+import "github.com/ZyperX/zbxss/internal/runner"
 
 func main() {
-scanner := bufio.NewScanner(os.Stdin)
-payload := os.Args[1]
-for scanner.Scan() {
-		target := strings.TrimSpace(scanner.Text())
-                probes(payload,target)
-}
-
+    runner.New()
 }
